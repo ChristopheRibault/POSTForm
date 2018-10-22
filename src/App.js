69c6452333a70ead0461ee5c64e66b115da7562b
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-22 16:18:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      url: '',
+      comm: '(pourquoi tu aimes ce film? qu\'est ce qui t\'a marqué? etc...)'
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    const url = 'http://92.175.11.66:3001/api/quests/movies/'
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+     };
+    fetch(url, config)
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          alert(res.error);
+        } else {
+          alert(`Film ajouté avec l'ID ${res}!`);
+        }
+      }).catch(e => {
+        console.error(e);
+        alert('Erreur lors de l\'ajout d\'un film');
+      })
+   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.submitForm} >
+          <label htmlFor='name'>Nom du film : 
+            <input onChange={this.onChange} value={this.state.name} type='text' id= 'name' name='name' />
+          </label><br/>
+          
+          <label htmlFor='url'>URL du poster : 
+            <input onChange={this.onChange} value={this.state.url} type='text' id='url' name='url' />
+          </label><br/>
+          
+          <label htmlFor='comm'>Commentaire :</label>
+          <textarea onChange={this.onChange} id='comm' name='comm' rows='10' cols='50' value={this.state.comm}></textarea><br/>
+
+          <input type='submit' value='Envoyer'/>
+        </form>
       </div>
     );
   }
